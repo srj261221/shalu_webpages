5363088cdc886803f9e352ff109f17e760295fe9
shalini <srj261221@gmail.com>
2022-06-09 14:04:22 +0530

    updated webpages

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//document.write("this is an external js2");
+//document.getElementById('th').innerHTML = "This is a para";
+
+var a1 = 45;
+var a11 = 10;
+var a2 =  "this is me";
+document.write(a1+a11);
+console.log("this is console");
